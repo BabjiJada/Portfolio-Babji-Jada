d79207c81237f9fa96be071b22d6e4ea5c9a250d
Babjijada <babjijada009@gmail.com>
2025-02-10 22:53:58 +0530

    Initial commit

---
+++ CODING/nextjs/Portfolio/nextjs-portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,83 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12 relative">
+        {/* Name Positioned in Rounded Area */}
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-primary-400 to-secondary-600 text-white rounded-full px-6 py-3 shadow-lg text-xl sm:text-3xl font-extrabold z-10">
+          {/* Babji Jada */}
+        </div>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <TypeAnimation
+              sequence={[
+                "Web Developer",
+                1000,
+                "Mobile Dev",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Babji Jada is a passionate software developer skilled in web development, backend engineering, and cloud computing, with experience in building scalable applications using technologies like React, Node.js, Spring Boot, and AWS. With a strong leadership background as a class representative and active involvement in community initiatives, Babji is dedicated to developing innovative tech solutions while continuously expanding expertise in software engineering.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/Babji_Jada_Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              download="Babji_Jada_Resume.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+              priority
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ CODING/nextjs/Portfolio/nextjs-portfolio/src/app/components/ProjectsSection.jsx
@@ -0,0 +1,128 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Project 1 description",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Railway Management System",
+    description: "The Railway Management System is a full-stack web application designed to streamline railway operations, including ticket booking, train scheduling, and passenger management. Built with HTML, CSS, PHP, and MySQL, this system offers an intuitive user interface that ensures a seamless experience for both passengers and railway staff.",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/BabjiJada/Railway-Management-System-using-Php.git",
+    previewUrl: "https://github.com/BabjiJada/Railway-Management-System-using-Php.git",
+  },
+  {
+    id: 3,
+    title: "Automated Code Reviewer & PR Analysis Tool",
+    description: "The Automated Code Reviewer & PR Analysis Tool is a React and Next.js-based web application designed to streamline and automate the process of reviewing pull requests (PRs). Leveraging the GitHub API and Python-based natural language processing (NLP) techniques, this tool scans PRs in real-time to identify potential issues, ensuring higher code quality and faster review cycles.",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "High-Performance URL Shortener",
+    description: "The High-Performance URL Shortener is a scalable service built with Java (Spring Boot), PostgreSQL, Redis, and Docker, designed to handle millions of concurrent requests with high availability and low latency. This URL shortening service allows users to create short, shareable links while providing valuable insights into user interactions and traffic patterns.",
+    image: "/images/projects/4.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  // {
+  //   id: 5,
+  //   title: "React Firebase Template",
+  //   description: "Authentication and CRUD operations",
+  //   image: "/images/projects/5.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "/",
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 6,
+  //   title: "Full-stack Roadmap",
+  //   description: "Project 5 description",
+  //   image: "/images/projects/6.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "/",
+  //   previewUrl: "/",
+  // },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
